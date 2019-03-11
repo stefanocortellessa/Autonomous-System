6a9c679d57eda408527e6a4c1eb92f48779733fb
Stefano Cortellessa <kort.94@hotmail.it>
2019-03-11 11:17:46 +0100

    Executor created

---
+++ sg.executor/src/main/java/sg/executor/ExecutorActivator.java
@@ -0,0 +1,10 @@
+package sg.executor;
+
+public class ExecutorActivator{
+	public static void main(String[] args) throws InterruptedException {
+			
+		Executor executor = new Executor();
+		
+		executor.start();			
+		}
+	}

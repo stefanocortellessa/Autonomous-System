7bc883b0d50a34da14f5baf957c2faf10652d646
Andrea <andreaperna24@gmail.com>
2019-03-29 22:11:26 +0100

    Upload Final Final Version

--- sg.simulator/src/main/java/sg/simulator/SimulatorActivator.java
+++
@@ -1,9 +0,0 @@
-package sg.simulator;
-
-public class SimulatorActivator{
-	public static void main(String[] args) throws InterruptedException {
-		
-		Simulator simulator = new Simulator();
-		simulator.start();
-	}
-}
